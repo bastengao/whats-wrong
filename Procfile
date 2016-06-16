bdd95753032a58f095b07fa68ef710ff64633a39
bastengao <bastengao@gmail.com>
2016-06-16 23:05:18 +0800

    Generator could gen static pages for specific locale.

---
+++ Procfile
@@ -0,0 +1,2 @@
+push-locale: localeapp -k $LOCALEAPP_KEY push lib/generators/whats_wrong/locales
+pull-locale: localeapp -k $LOCALEAPP_KEY pull lib/generators/whats_wrong/locales
--- lib/generators/whats_wrong/pages_generator.rb
+++ lib/generators/whats_wrong/pages_generator.rb
@@ -2,11 +2,34 @@ require 'rails/generators'
 
 module WhatsWrong
   module Generators
-    class PagesGenerator < Rails::Generators::Base
+    class PagesGenerator < Rails::Generators::NamedBase
+      source_root File.expand_path("../templates", __FILE__)
 
       def gen
+        locale = name
+        puts "Creating pages for #{locale} ..."
+
+        [404, 422, 500].each {|status| process(locale, status) }
+      rescue => e
+        puts e.message
+      end
+
+      protected
+      def process(locale, status)
         # using: https://github.com/rails/rails/tree/master/railties/lib/rails/generators/rails/app/templates/public
-        # TODO
+
+        @locales = locales(locale)['pages'][status.to_s]
+        template "#{status}.html", "public/#{status}.#{locale}.html"
+      end
+
+      def locales(locale)
+        locale_file = File.expand_path("../locales/#{locale}.yml", __FILE__)
+        if File.exist?(locale_file)
+          translations = YAML.load_file(locale_file)
+          translations[locale]
+        else
+          raise "The #{locale} is not supported currently. Contribute the translations if you can, visit https://www.localeapp.com/projects/9397 ."
+        end
       end
     end
   end
